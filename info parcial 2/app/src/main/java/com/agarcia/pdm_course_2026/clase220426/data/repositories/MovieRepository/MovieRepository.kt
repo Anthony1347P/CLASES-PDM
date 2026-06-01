878f36dcd413e06bdf6c60534aa58eb33e875ecb
Anthony <00202124@uca.edu.sv>
2026-05-31 22:18:34 -0600

    arreglos

--- info parcial 2/app/src/main/java/com/agarcia/pdm_course_2026/clase220426/data/repositories/MovieRepository/MovieRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.agarcia.pdm_course_2026.clase220426.data.repositories.MovieRepository
-
-import com.agarcia.pdm_course_2026.clase220426.model.Movie
-
-interface MovieRepository {
-  suspend fun getMovies(): Result<List<Movie>>
-  suspend fun getMovieById(id: Int): Result<Movie>
-}
--- info parcial 2/app/src/main/java/com/agarcia/pdm_course_2026/clase270426/model/OrderItem.kt
+++
@@ -1,7 +0,0 @@
-package com.agarcia.pdm_course_2026.clase270426.model
-
-data class OrderItem(
-  val producto: Producto,
-  val cantidad: Int
-)
-
